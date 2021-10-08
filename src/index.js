dc88fb34f092f92d3953ee651eb7fecb4f551a36
Nathan Sellars <nsellars98@gmail.com>
2021-10-08 11:52:17 -0500

    More updates

--- src/index.js
+++ src/index.js
@@ -6,9 +6,19 @@ const inputElement = document.querySelector("#input-element");
 const inputButton = document.querySelector("#save-input-button");
 const unorderedListElement = document.querySelector("#unordered-list-element");
 
+// localStorage.clear();
+
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  processLeads();
+} // else, no leads to process, doNothing();
+
 inputButton?.addEventListener("click", function() {
   myLeads.push(inputElement?.value);
   inputElement.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
   processLeads();
 })
 
